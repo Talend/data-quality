a933d1b81d4cd8ba3e6b085115bdf9cb893d58d6
zshen <zshen@192.168.32.185>
2017-02-28 18:51:36 +0800

    feature TDQ-13262 add junit test case by jira description

--- dataquality-survivorship/src/test/java/org/talend/survivorship/SurvivorshipManagerTest.java
+++ dataquality-survivorship/src/test/java/org/talend/survivorship/SurvivorshipManagerTest.java
@@ -12,15 +12,17 @@
 // ============================================================================
 package org.talend.survivorship;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
+import java.util.Date;
+import java.util.HashSet;
 import java.util.Map;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.kie.internal.KnowledgeBase;
+import org.talend.survivorship.model.RuleDefinition;
 import org.talend.survivorship.sample.SampleData;
 
 /**
@@ -45,8 +47,8 @@ public class SurvivorshipManagerTest {
         for (String str : SampleData.COLUMNS.keySet()) {
             manager.addColumn(str, SampleData.COLUMNS.get(str));
         }
-        for (int i = 0; i < SampleData.RULES.length; i++) {
-            manager.addRuleDefinition(SampleData.RULES[i]);
+        for (RuleDefinition element : SampleData.RULES) {
+            manager.addRuleDefinition(element);
         }
     }
 
@@ -57,10 +59,10 @@ public class SurvivorshipManagerTest {
     public void testInitKnowledgeBase() {
         manager.initKnowledgeBase();
         KnowledgeBase base = manager.getKnowledgeBase();
-        assertNotNull("Model is null", base.getFactType(SampleData.PKG_NAME, "RecordIn"));
+        assertNotNull("Model is null", base.getFactType(SampleData.PKG_NAME, "RecordIn")); //$NON-NLS-1$ //$NON-NLS-2$
 
         assertNotNull(base.getRule(SampleData.PKG_NAME, SampleData.RULES[0].getRuleName()));
-        assertNotNull(base.getProcess(SampleData.PKG_NAME + ".SurvivorFlow"));
+        assertNotNull(base.getProcess(SampleData.PKG_NAME + ".SurvivorFlow")); //$NON-NLS-1$
 
     }
 
@@ -76,7 +78,7 @@ public class SurvivorshipManagerTest {
         for (String col : SampleData.COLUMNS.keySet()) {
             assertEquals(SampleData.EXPECTED_SURVIVOR.get(col), survivors.get(col));
         }
-        assertTrue("conflicts are not the same as expected.",
+        assertTrue("conflicts are not the same as expected.", //$NON-NLS-1$
                 manager.getConflictsOfSurvivor().equals(SampleData.EXPECTED_CONFLICT_OF_SURVIVOR));
 
         // Run the same test for a second time
@@ -86,7 +88,173 @@ public class SurvivorshipManagerTest {
         for (String col : SampleData.COLUMNS.keySet()) {
             assertEquals(SampleData.EXPECTED_SURVIVOR.get(col), survivors2.get(col));
         }
-        assertTrue("conflicts are not the same as expected.",
+        assertTrue("conflicts are not the same as expected.", //$NON-NLS-1$
                 manager.getConflictsOfSurvivor().equals(SampleData.EXPECTED_CONFLICT_OF_SURVIVOR));
     }
+
+    /**
+     * Test method for {@link org.talend.survivorship.SurvivorshipManager#runSession(java.lang.String[][])}.
+     * 
+     * @case1 most frequent->most recent and with null
+     */
+
+    @Test
+    public void testRunSessionCase1() {
+
+        manager = new SurvivorshipManager(SampleData.RULE_PATH, SampleData.PKG_NAME_CONFLICT);
+
+        for (String str : SampleData.COLUMNS_CONFLICT.keySet()) {
+            manager.addColumn(str, SampleData.COLUMNS_CONFLICT.get(str));
+        }
+        for (RuleDefinition element : SampleData.RULES_CONFLICT) {
+            manager.addRuleDefinition(element);
+        }
+        manager.initKnowledgeBase();
+        manager.runSession(SampleData.SAMPLE_INPUT_CONFLICT);
+        // 5. Retrieve results
+        HashSet<String> conflictsOfSurvivor = manager.getConflictsOfSurvivor();
+        Assert.assertEquals("The size of conflictsOfSurvivor should be 1", 1, conflictsOfSurvivor.size()); //$NON-NLS-1$
+        Assert.assertTrue("The column of conflict should be birthday", conflictsOfSurvivor.contains("birthday")); //$NON-NLS-1$ //$NON-NLS-2$
+        Map<String, Object> survivorMap = manager.getSurvivorMap();
+        Assert.assertTrue("The SurvivorMap should not be null", survivorMap != null); //$NON-NLS-1$ 
+        Object birthdayObj = survivorMap.get("birthday"); //$NON-NLS-1$
+        Assert.assertTrue("The birthdayObj should not be null", birthdayObj != null); //$NON-NLS-1$ 
+        Date resultDate = (Date) birthdayObj;
+        Assert.assertEquals("The resultDate should be 08-08-2000", "08-08-2000", //$NON-NLS-1$ //$NON-NLS-2$
+                SampleData.dateToString(resultDate, "dd-MM-yyyy")); //$NON-NLS-1$
+    }
+
+    /**
+     * Test method for {@link org.talend.survivorship.SurvivorshipManager#runSession(java.lang.String[][])}.
+     * 
+     * @case2 most frequent->longest and with null
+     */
+
+    @Test
+    public void testRunSessionCase2() {
+
+        manager = new SurvivorshipManager(SampleData.RULE_PATH, SampleData.PKG_NAME_CONFLICT_FRE_LONG);
+
+        for (String str : SampleData.COLUMNS_CONFLICT.keySet()) {
+            manager.addColumn(str, SampleData.COLUMNS_CONFLICT.get(str));
+        }
+        for (RuleDefinition element : SampleData.RULES_CONFLICT_FRE_LONG) {
+            manager.addRuleDefinition(element);
+        }
+        manager.initKnowledgeBase();
+        manager.runSession(SampleData.SAMPLE_INPUT_CONFLICT);
+        // 5. Retrieve results
+        HashSet<String> conflictsOfSurvivor = manager.getConflictsOfSurvivor();
+        Assert.assertEquals("The size of conflictsOfSurvivor should be 1", 1, conflictsOfSurvivor.size()); //$NON-NLS-1$
+        Assert.assertTrue("The column of conflict should be birthday", conflictsOfSurvivor.contains("city")); //$NON-NLS-1$ //$NON-NLS-2$
+        Map<String, Object> survivorMap = manager.getSurvivorMap();
+        Assert.assertTrue("The SurvivorMap should not be null", survivorMap != null); //$NON-NLS-1$ 
+        Object cityObj = survivorMap.get("city"); //$NON-NLS-1$
+        Assert.assertTrue("The birthdayObj should not be null", cityObj != null); //$NON-NLS-1$ 
+        String resultStr = (String) cityObj;
+        Assert.assertEquals("The resultStr should be shanghai", "shanghai", //$NON-NLS-1$ //$NON-NLS-2$
+                resultStr);
+    }
+
+    /**
+     * Test method for {@link org.talend.survivorship.SurvivorshipManager#runSession(java.lang.String[][])}.
+     * 
+     * @case3 most frequent->longest->recent and with null
+     */
+
+    @Test
+    public void testRunSessionCase3() {
+
+        manager = new SurvivorshipManager(SampleData.RULE_PATH, SampleData.PKG_NAME_CONFLICT_FRE_LONG_RECENT);
+
+        for (String str : SampleData.COLUMNS_CONFLICT.keySet()) {
+            manager.addColumn(str, SampleData.COLUMNS_CONFLICT.get(str));
+        }
+        for (RuleDefinition element : SampleData.RULES_CONFLICT_FRE_LONG_RECENT) {
+            manager.addRuleDefinition(element);
+        }
+        manager.initKnowledgeBase();
+        manager.runSession(SampleData.SAMPLE_INPUT_CONFLICT);
+        // 5. Retrieve results
+        HashSet<String> conflictsOfSurvivor = manager.getConflictsOfSurvivor();
+        Assert.assertEquals("The size of conflictsOfSurvivor should be 1", 1, conflictsOfSurvivor.size()); //$NON-NLS-1$
+        Assert.assertTrue("The column of conflict should be birthday", conflictsOfSurvivor.contains("firstName")); //$NON-NLS-1$ //$NON-NLS-2$
+        Map<String, Object> survivorMap = manager.getSurvivorMap();
+        Assert.assertTrue("The SurvivorMap should not be null", survivorMap != null); //$NON-NLS-1$ 
+        Object firstNameObj = survivorMap.get("firstName"); //$NON-NLS-1$
+        Assert.assertTrue("The firstNameObj should not be null", firstNameObj != null); //$NON-NLS-1$ 
+        String resultStr = (String) firstNameObj;
+        Assert.assertEquals("The resultStr should be Tony", "Tony", //$NON-NLS-1$ //$NON-NLS-2$
+                resultStr);
+    }
+
+    /**
+     * Test method for {@link org.talend.survivorship.SurvivorshipManager#runSession(java.lang.String[][])}.
+     * 
+     * @case4 most frequent->null->constant
+     * 
+     * the constant is Green
+     * other column is firstName column
+     */
+
+    @Test
+    public void testRunSessionCase4() {
+
+        manager = new SurvivorshipManager(SampleData.RULE_PATH, SampleData.PKG_NAME_CONFLICT_FRE_NULL_CONSTANT);
+
+        for (String str : SampleData.COLUMNS_CONFLICT.keySet()) {
+            manager.addColumn(str, SampleData.COLUMNS_CONFLICT.get(str));
+        }
+        for (RuleDefinition element : SampleData.RULES_CONFLICT_FRE_NULL_CONTSTANT) {
+            manager.addRuleDefinition(element);
+        }
+        manager.initKnowledgeBase();
+        manager.runSession(SampleData.SAMPLE_INPUT_CONFLICT);
+        // 5. Retrieve results
+        // HashSet<String> conflictsOfSurvivor = manager.getConflictsOfSurvivor();
+        //        Assert.assertEquals("The size of conflictsOfSurvivor should be 1", 1, conflictsOfSurvivor.size()); //$NON-NLS-1$
+        //        Assert.assertTrue("The column of conflict should be lastName", conflictsOfSurvivor.contains("lastName")); //$NON-NLS-1$ //$NON-NLS-2$
+        Map<String, Object> survivorMap = manager.getSurvivorMap();
+        Assert.assertTrue("The SurvivorMap should not be null", survivorMap != null); //$NON-NLS-1$ 
+        Object lastNameObj = survivorMap.get("lastName"); //$NON-NLS-1$
+        Assert.assertTrue("The lastNameObj should not be null", lastNameObj != null); //$NON-NLS-1$ 
+        String resultStr = (String) lastNameObj;
+        Assert.assertEquals("The resultStr should be Tony", "Tony", //$NON-NLS-1$ //$NON-NLS-2$
+                resultStr);
+    }
+
+    /**
+     * Test method for {@link org.talend.survivorship.SurvivorshipManager#runSession(java.lang.String[][])}.
+     * 
+     * @case5 most frequent->null->frequent
+     * 
+     * other column is lastName column
+     */
+
+    @Test
+    public void testRunSessionCase5() {
+
+        manager = new SurvivorshipManager(SampleData.RULE_PATH, SampleData.PKG_NAME_CONFLICT_TWO_TARGET_ONE_COLUMN);
+
+        for (String str : SampleData.COLUMNS_CONFLICT.keySet()) {
+            manager.addColumn(str, SampleData.COLUMNS_CONFLICT.get(str));
+        }
+        for (RuleDefinition element : SampleData.RULES_CONFLICT_TWO_TARGET_ONE_COLUMN) {
+            manager.addRuleDefinition(element);
+        }
+        manager.initKnowledgeBase();
+        manager.runSession(SampleData.SAMPLE_INPUT_CONFLICT);
+        // 5. Retrieve results
+        HashSet<String> conflictsOfSurvivor = manager.getConflictsOfSurvivor();
+        Assert.assertEquals("The size of conflictsOfSurvivor should be 0", 0, conflictsOfSurvivor.size()); //$NON-NLS-1$
+        Map<String, Object> survivorMap = manager.getSurvivorMap();
+        Assert.assertTrue("The SurvivorMap should not be null", survivorMap != null); //$NON-NLS-1$ 
+        Assert.assertTrue("The size of SurvivorMap should be 1", survivorMap.size() == 1); //$NON-NLS-1$
+        Object birthdayObj = survivorMap.get("birthday"); //$NON-NLS-1$
+        Assert.assertTrue("The birthdayNameObj should not be null", birthdayObj != null); //$NON-NLS-1$ 
+        String resultDate = SampleData.dateToString((Date) birthdayObj, "dd-MM-yyyy"); //$NON-NLS-1$
+        Assert.assertEquals("The resultDate should be 08-08-2000", "08-08-2000", //$NON-NLS-1$ //$NON-NLS-2$
+                resultDate);
+    }
+
 }
--- dataquality-survivorship/src/test/java/org/talend/survivorship/sample/SampleData.java
+++ dataquality-survivorship/src/test/java/org/talend/survivorship/sample/SampleData.java
@@ -28,68 +28,130 @@ import org.talend.survivorship.model.RuleDefinition.Order;
  */
 public final class SampleData {
 
-    public static final String RULE_PATH = "src/test/resources/generated/";
+    public static final String RULE_PATH = "src/test/resources/generated/"; //$NON-NLS-1$
 
-    public static final String PKG_NAME = "org.talend.survivorship.sample";
+    public static final String PKG_NAME = "org.talend.survivorship.sample"; //$NON-NLS-1$
+
+    public static final String PKG_NAME_CONFLICT = "org.talend.survivorship.conflict"; //$NON-NLS-1$
+
+    public static final String PKG_NAME_CONFLICT_FRE_LONG = "org.talend.survivorship.conflict.fre_long"; //$NON-NLS-1$
+
+    public static final String PKG_NAME_CONFLICT_FRE_LONG_RECENT = "org.talend.survivorship.conflict.fre_long_recent"; //$NON-NLS-1$
+
+    public static final String PKG_NAME_CONFLICT_FRE_NULL_CONSTANT = "org.talend.survivorship.conflict.fre_null_constant"; //$NON-NLS-1$
+
+    public static final String PKG_NAME_CONFLICT_FRE_NULL_FRE = "org.talend.survivorship.conflict.fre_null_fre"; //$NON-NLS-1$
+
+    public static final String PKG_NAME_CONFLICT_TWO_TARGET_ONE_COLUMN = "org.talend.survivorship.conflict.two_target_one_column"; //$NON-NLS-1$
 
     public static final Object[][] SAMPLE_INPUT = {
-            { "GRIZZARD CO.", "110 N MARYLAND AVE", "GLENDALE", "CA", "912066", "FR", "8185431314",
-                    stringToDate("20110101", "yyyyMMdd"), 1.0, 18, 1985, "Something" },
-            { "GRIZZARD", "110 NORTH MARYLAND AVENUE", "GLENDALE", "CA", "91205", "US", "9003254892",
-                    stringToDate("20110118", "yyyyMMdd"), 0.9879999999999999, 25, 0, "" },
-            { "GRIZZARD INC", "110 N. MARYLAND AVENUE", "GLENDALE", "CA", "91206", "US", "(818) 543-1315",
-                    stringToDate("20110103", "yyyyMMdd"), 0.8572727272727272, 31, 1970, null },
-            { "GRIZZARD CO", "1480 S COLORADO BOULEVARD", "LOS ANGELES", "CA", "91206", "US", "(800) 325-4892",
-                    stringToDate("20110115", "yyyyMMdd"), 0.742319482, 35, 0, null } };
+            { "GRIZZARD CO.", "110 N MARYLAND AVE", "GLENDALE", "CA", "912066", "FR", "8185431314", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-7$
+                    stringToDate("20110101", "yyyyMMdd"), 1.0, 18, 1985, "Something" }, //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+            { "GRIZZARD", "110 NORTH MARYLAND AVENUE", "GLENDALE", "CA", "91205", "US", "9003254892", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-7$
+                    stringToDate("20110118", "yyyyMMdd"), 0.9879999999999999, 25, 0, "" }, //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+            { "GRIZZARD INC", "110 N. MARYLAND AVENUE", "GLENDALE", "CA", "91206", "US", "(818) 543-1315", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-7$
+                    stringToDate("20110103", "yyyyMMdd"), 0.8572727272727272, 31, 1970, null }, //$NON-NLS-1$ //$NON-NLS-2$
+            { "GRIZZARD CO", "1480 S COLORADO BOULEVARD", "LOS ANGELES", "CA", "91206", "US", "(800) 325-4892", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-7$
+                    stringToDate("20110115", "yyyyMMdd"), 0.742319482, 35, 0, null } }; //$NON-NLS-1$ //$NON-NLS-2$
+
+    public static final Object[][] SAMPLE_INPUT_CONFLICT = {
+            { "Lili", "Cheng", "Walker", "beijing", 1, stringToDate("04-04-2000", "dd-MM-yyyy"), "1", 7 }, //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-7$
+            { "Tony", null, "Martain", "shanghai", 1, stringToDate("06-06-2000", "dd-MM-yyyy"), "1", 0 }, //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ 
+            { "Tom", "Green", null, "shanghai", 3, stringToDate("04-04-2000", "dd-MM-yyyy"), "1", 0 }, //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$
+            { "san", "Green", null, "hebeihebei", 2, stringToDate("08-08-2000", "dd-MM-yyyy"), "1", 0 }, //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$
+            { "li", "Brown", "Joseph", "beijing", 2, stringToDate("08-08-2000", "dd-MM-yyyy"), "1", 0 }, //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-7$
+            { "Tony", null, "William", null, 2, null, "1", 0 }, //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ 
+            { "Lili", "si", "Allen", null, 2, null, "1", 0 } }; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ 
 
     public static final LinkedHashMap<String, String> COLUMNS = new LinkedHashMap<String, String>() {
 
         private static final long serialVersionUID = 1L;
         {
-            put("acctName", "String");
-            put("addr", "String");
-            put("city", "String");
-            put("state", "String");
-            put("zip", "String");
-            put("country", "String");
-            put("phone", "String");
-            put("date", "java.util.Date");
-            put("score", "double");
-            put("age", "int");
-            put("birthyear", "int");
-            put("completeness", "String");
+            put("acctName", "String"); //$NON-NLS-1$ //$NON-NLS-2$
+            put("addr", "String"); //$NON-NLS-1$ //$NON-NLS-2$
+            put("city", "String"); //$NON-NLS-1$ //$NON-NLS-2$
+            put("state", "String"); //$NON-NLS-1$ //$NON-NLS-2$
+            put("zip", "String"); //$NON-NLS-1$ //$NON-NLS-2$
+            put("country", "String"); //$NON-NLS-1$ //$NON-NLS-2$
+            put("phone", "String"); //$NON-NLS-1$ //$NON-NLS-2$
+            put("date", "java.util.Date"); //$NON-NLS-1$ //$NON-NLS-2$
+            put("score", "double"); //$NON-NLS-1$ //$NON-NLS-2$
+            put("age", "int"); //$NON-NLS-1$ //$NON-NLS-2$
+            put("birthyear", "int"); //$NON-NLS-1$ //$NON-NLS-2$
+            put("completeness", "String"); //$NON-NLS-1$ //$NON-NLS-2$
+        }
+    };
+
+    public static final LinkedHashMap<String, String> COLUMNS_CONFLICT = new LinkedHashMap<String, String>() {
+
+        private static final long serialVersionUID = 1L;
+        {
+
+            put("firstName", "String"); //$NON-NLS-1$ //$NON-NLS-2$
+            put("lastName", "String"); //$NON-NLS-1$ //$NON-NLS-2$
+            put("middleName", "String"); //$NON-NLS-1$ //$NON-NLS-2$
+            put("city", "String"); //$NON-NLS-1$ //$NON-NLS-2$
+            put("id", "Integer"); //$NON-NLS-1$ //$NON-NLS-2$
+            put("birthday", "java.util.Date"); //$NON-NLS-1$ //$NON-NLS-2$
+            put("gid", "String"); //$NON-NLS-1$ //$NON-NLS-2$
+            put("grp_size", "Integer"); //$NON-NLS-1$ //$NON-NLS-2$
+
         }
     };
 
     public static final RuleDefinition[] RULES = {
-            new RuleDefinition(Order.SEQ, "CompletenessRule", null, Function.MostComplete, null, "completeness", true),
-            new RuleDefinition(Order.SEQ, "LengthAcct", "acctName", Function.Expression, ".length > 11", "acctName", true),
-            new RuleDefinition(Order.SEQ, "LongestAddr", "addr", Function.Longest, null, "addr", true),
-            new RuleDefinition(Order.SEQ, "HighScore", "score", Function.Expression, " > 0.95", "score", true),
-            new RuleDefinition(Order.SEQ, "MostCommonCity", "city", Function.MostCommon, null, "city", true),
-            new RuleDefinition(Order.SEQ, "MostCommonZip", "zip", Function.MostCommon, null, "zip", true),
+            new RuleDefinition(Order.SEQ, "CompletenessRule", null, Function.MostComplete, null, "completeness", true), //$NON-NLS-1$ //$NON-NLS-2$
+            new RuleDefinition(Order.SEQ, "LengthAcct", "acctName", Function.Expression, ".length > 11", "acctName", true), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+            new RuleDefinition(Order.SEQ, "LongestAddr", "addr", Function.Longest, null, "addr", true), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+            new RuleDefinition(Order.SEQ, "HighScore", "score", Function.Expression, " > 0.95", "score", true), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+            new RuleDefinition(Order.SEQ, "MostCommonCity", "city", Function.MostCommon, null, "city", true), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+            new RuleDefinition(Order.SEQ, "MostCommonZip", "zip", Function.MostCommon, null, "zip", true), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
             // new RuleDefinition(Order.MC, "ZipRegex", "zip", Function.MatchRegex, "\\\\d{5}",null, true),
-            new RuleDefinition(Order.MT, null, null, null, null, "state", true),
-            new RuleDefinition(Order.MT, null, null, null, null, "country", true),
-            new RuleDefinition(Order.SEQ, "LatestPhone", "date", Function.MostRecent, null, "date", true),
-            new RuleDefinition(Order.MT, null, null, null, null, "phone", true) };
+            new RuleDefinition(Order.MT, null, null, null, null, "state", true), //$NON-NLS-1$
+            new RuleDefinition(Order.MT, null, null, null, null, "country", true), //$NON-NLS-1$
+            new RuleDefinition(Order.SEQ, "LatestPhone", "date", Function.MostRecent, null, "date", true), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+            new RuleDefinition(Order.MT, null, null, null, null, "phone", true) }; //$NON-NLS-1$
+
+    public static final RuleDefinition[] RULES_CONFLICT = { new RuleDefinition(Order.SEQ, "more_common_birthday", "birthday", //$NON-NLS-1$ //$NON-NLS-2$
+            Function.MostCommon, null, "birthday", false) }; //$NON-NLS-1$
+
+    public static final RuleDefinition[] RULES_CONFLICT_FRE_LONG = { new RuleDefinition(Order.SEQ, "more_common_city", "city", //$NON-NLS-1$ //$NON-NLS-2$
+            Function.MostCommon, null, "city", false) }; //$NON-NLS-1$
+
+    public static final RuleDefinition[] RULES_CONFLICT_FRE_LONG_RECENT = {
+            new RuleDefinition(Order.SEQ, "more_common_firstName", "firstName", //$NON-NLS-1$ //$NON-NLS-2$
+                    Function.MostCommon, null, "firstName", false) }; //$NON-NLS-1$
+
+    public static final RuleDefinition[] RULES_CONFLICT_FRE_NULL_CONTSTANT = {
+            new RuleDefinition(Order.SEQ, "more_common_lastName", "lastName", //$NON-NLS-1$ //$NON-NLS-2$
+                    Function.MostCommon, null, "lastName", false) }; //$NON-NLS-1$
+
+    public static final RuleDefinition[] RULES_CONFLICT_FRE_NULL_FRE = {
+            new RuleDefinition(Order.SEQ, "more_common_middleName", "middleName", //$NON-NLS-1$ //$NON-NLS-2$
+                    Function.MostCommon, null, "middleName", false) }; //$NON-NLS-1$
+
+    public static final RuleDefinition[] RULES_CONFLICT_TWO_TARGET_ONE_COLUMN = {
+            new RuleDefinition(Order.SEQ, "more_recent_birthday", "birthday", //$NON-NLS-1$ //$NON-NLS-2$
+                    Function.MostRecent, null, "birthday", false), //$NON-NLS-1$
+            new RuleDefinition(Order.SEQ, "Longest_birthday", "city", //$NON-NLS-1$ //$NON-NLS-2$
+                    Function.Longest, null, "birthday", false) }; //$NON-NLS-1$
 
     public static final HashMap<String, Object> EXPECTED_SURVIVOR = new HashMap<String, Object>() {
 
         private static final long serialVersionUID = 1L;
         {
-            put("acctName", "GRIZZARD CO.");
-            put("addr", "110 NORTH MARYLAND AVENUE");
-            put("city", "GLENDALE");
-            put("state", "CA");
-            put("zip", "91206");
-            put("country", "US");
-            put("phone", "9003254892");
-            put("date", stringToDate("20110118", "yyyyMMdd"));
-            put("score", 1.0);
-            put("age", null);
-            put("birthyear", null);
-            put("completeness", "Something");
+            put("acctName", "GRIZZARD CO."); //$NON-NLS-1$ //$NON-NLS-2$
+            put("addr", "110 NORTH MARYLAND AVENUE"); //$NON-NLS-1$ //$NON-NLS-2$
+            put("city", "GLENDALE"); //$NON-NLS-1$ //$NON-NLS-2$
+            put("state", "CA"); //$NON-NLS-1$ //$NON-NLS-2$
+            put("zip", "91206"); //$NON-NLS-1$ //$NON-NLS-2$
+            put("country", "US"); //$NON-NLS-1$ //$NON-NLS-2$
+            put("phone", "9003254892"); //$NON-NLS-1$ //$NON-NLS-2$
+            put("date", stringToDate("20110118", "yyyyMMdd")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+            put("score", 1.0); //$NON-NLS-1$
+            put("age", null); //$NON-NLS-1$
+            put("birthyear", null); //$NON-NLS-1$
+            put("completeness", "Something"); //$NON-NLS-1$ //$NON-NLS-2$
         }
     };
 
@@ -97,9 +159,9 @@ public final class SampleData {
 
         private static final long serialVersionUID = 1L;
         {
-            add("acctName");
-            add("score");
-            add("addr");
+            add("acctName"); //$NON-NLS-1$
+            add("score"); //$NON-NLS-1$
+            add("addr"); //$NON-NLS-1$
         }
     };
 
