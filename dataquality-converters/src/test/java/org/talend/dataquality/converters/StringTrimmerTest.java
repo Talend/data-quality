eebf60d2b39b2f31c879e672b578523a095d89b3
Jules Teulade-Denantes <jteuladedenantes@talend.com>
2017-09-18 15:08:32 +0200

    chore(TDQ-14048): improve StringTrimmer

--- dataquality-converters/src/test/java/org/talend/dataquality/converters/StringTrimmerTest.java
+++ dataquality-converters/src/test/java/org/talend/dataquality/converters/StringTrimmerTest.java
@@ -25,11 +25,39 @@ import org.junit.Test;
 
 /**
  * Test for class {@link StringTrimmer}.
- * 
+ *
  * @author msjian
  * @version 2017.02.08
  */
 public class StringTrimmerTest {
+
+    public String[] WHITESPACE_CHARS = new String[] { "\t" // CHARACTER TABULATION //$NON-NLS-1$
+            , "\n" // LINE FEED (LF) //$NON-NLS-1$
+            , '\u000B' + "" // LINE TABULATION //$NON-NLS-1$
+            , "\f" // FORM FEED (FF) //$NON-NLS-1$
+            , "\r" // CARRIAGE RETURN (CR) //$NON-NLS-1$
+            , " " // SPACE //$NON-NLS-1$
+            , '\u0085' + "" // NEXT LINE (NEL) //$NON-NLS-1$
+            , '\u00A0' + "" // NO-BREAK SPACE //$NON-NLS-1$
+            , '\u1680' + "" // OGHAM SPACE MARK //$NON-NLS-1$
+            , '\u180E' + "" // MONGOLIAN VOWEL SEPARATOR //$NON-NLS-1$
+            , '\u2000' + "" // EN QUAD //$NON-NLS-1$
+            , '\u2001' + "" // EM QUAD //$NON-NLS-1$
+            , '\u2002' + "" // EN SPACE //$NON-NLS-1$
+            , '\u2003' + "" // EM SPACE //$NON-NLS-1$
+            , '\u2004' + "" // THREE-PER-EM SPACE //$NON-NLS-1$
+            , '\u2005' + "" // FOUR-PER-EM SPACE //$NON-NLS-1$
+            , '\u2006' + "" // SIX-PER-EM SPACE //$NON-NLS-1$
+            , '\u2007' + "" // FIGURE SPACE //$NON-NLS-1$
+            , '\u2008' + "" // PUNCTUATION SPACE //$NON-NLS-1$
+            , '\u2009' + "" // THIN SPACE //$NON-NLS-1$
+            , '\u200A' + "" // HAIR SPACE //$NON-NLS-1$
+            , '\u2028' + "" // LINE SEPARATOR //$NON-NLS-1$
+            , '\u2029' + "" // PARAGRAPH SEPARATOR //$NON-NLS-1$
+            , '\u202F' + "" // NARROW NO-BREAK SPACE //$NON-NLS-1$
+            , '\u205F' + "" // MEDIUM MATHEMATICAL SPACE //$NON-NLS-1$
+            , '\u3000' + "" // IDEOGRAPHIC SPACE //$NON-NLS-1$
+    };
 
     private static final String expected = "abc"; //$NON-NLS-1$
 
@@ -67,7 +95,7 @@ public class StringTrimmerTest {
         assertEquals("a" + "\t" + "bc", stringTrimmer.removeTrailingAndLeading("\t" + "a" + "\t" + "bc", "\t")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-7$ //$NON-NLS-8$
         assertEquals("\t" + expected, stringTrimmer.removeTrailingAndLeading("\t" + "abc ")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
         assertEquals(expected, ("\t" + "abc ").trim()); //$NON-NLS-1$ //$NON-NLS-2$
-                                                        // //$NON-NLS-3
+        // //$NON-NLS-3
 
         assertEquals(expected, stringTrimmer.removeTrailingAndLeading("\n" + expected, "\n")); //$NON-NLS-1$ //$NON-NLS-2$
         assertEquals("abc ", stringTrimmer.removeTrailingAndLeading("\n" + "abc ", "\n")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
@@ -87,7 +115,7 @@ public class StringTrimmerTest {
     public void testRemoveTrailingAndLeadingWhitespaces() {
         StringTrimmer stringTrimmer = new StringTrimmer();
         String inputData = " " + expected; //$NON-NLS-1$
-        for (String removechar : StringTrimmer.WHITESPACE_CHARS) {
+        for (String removechar : WHITESPACE_CHARS) {
             inputData = inputData + removechar;
         }
         assertEquals(expected, stringTrimmer.removeTrailingAndLeadingWhitespaces(inputData));
@@ -160,11 +188,11 @@ public class StringTrimmerTest {
 
         String result = inputStr; // original
         // String result = inputStr.trim(); // pull request from davkrause
-        while (StringUtils.startsWithAny(result, StringTrimmer.WHITESPACE_CHARS)) {
+        while (StringUtils.startsWithAny(result, WHITESPACE_CHARS)) {
             result = StringUtils.removeStart(result, result.substring(0, 1));
         }
 
-        while (StringUtils.endsWithAny(result, StringTrimmer.WHITESPACE_CHARS)) {
+        while (StringUtils.endsWithAny(result, WHITESPACE_CHARS)) {
             result = StringUtils.removeEnd(result, result.substring(result.length() - 1, result.length()));
         }
         return result;
