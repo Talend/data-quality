3a6ce7766ea2a19a881217d38b6eb210b9b3b994
Fangrou <fji@talend.com>
2018-06-19 15:05:21 +0200

    feat(TDQ-14587): api japanese transliteration

---
+++ dataquality-text-japanese/src/main/java/org/talend/dataquality/jp/transliteration/TextTransliterator.java
@@ -0,0 +1,107 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2018 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.dataquality.jp.transliteration;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.talend.dataquality.jp.tokenization.TextTokenizer;
+
+import com.atilika.kuromoji.TokenBase;
+
+public class TextTransliterator {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TextTransliterator.class);
+
+    private static final int READING_ID = 7;
+
+    private static final int PRONUNCIATION_ID = 8;
+
+    private static TextTokenizer textTokenizer;
+
+    public enum TransliterateType {
+        HIRAGANA("Hiragana"),
+        KATAKANA_READING("Katakana-reading"),
+        KATAKANA_PRONUNCIATION("Katakana-pronunciation"),
+        HEPBURN("Hepburn-romanization"),
+        KUNREI_SHIKI("Kunrei-shiki-romanization"),
+        NIHON_SHIKI("Nihon-shiki-romanization");
+
+        private final String typeName;
+
+        public String getTypeName() {
+            return typeName;
+        }
+
+        TransliterateType(String typeName) {
+            this.typeName = typeName;
+        }
+    }
+
+    private TextTransliterator() {
+        try {
+            textTokenizer = TextTokenizer.getInstance();
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+        }
+    }
+
+    private static class LazyHolder {
+
+        static final TextTransliterator INSTANCE = new TextTransliterator();
+    }
+
+    public static TextTransliterator getInstance() {
+        return LazyHolder.INSTANCE;
+    }
+
+    public String transliterate(String text, TransliterateType type) {
+        return transliterate(text, type, " ");
+    }
+
+    public String transliterate(String text, TransliterateType type, String delimiter) {
+        return convert(text, type).collect(Collectors.joining(delimiter));
+    }
+
+    private Stream<String> convert(String text, TransliterateType type) {
+        if (type.equals(TransliterateType.KATAKANA_READING) || type.equals(TransliterateType.HIRAGANA)) {
+            // if output type is KATAKANA_READING or HIRAGANA
+            final Stream<String> katakanaRStream = convert2Katakana(text, false);
+            if (type.equals(TransliterateType.KATAKANA_READING)) {
+                return katakanaRStream;
+            }
+            return KatakanaToHiragana.convert(katakanaRStream);
+            //Katakana reading
+        } else {
+            // if output type is KATAKANA_PRONUNCIATION or rōmaji, the text should be converted to Katakana pronunciation
+            final Stream<String> katakanaPStream = convert2Katakana(text, true);
+            if (type.equals(TransliterateType.KATAKANA_PRONUNCIATION)) {
+                return katakanaPStream;
+            }
+            return KatakanaToRomaji.convert(katakanaPStream, type);
+        }
+    }
+
+    private Stream<String> convert2Katakana(String text, Boolean toKatakanaPronunciation) {
+        final List<? extends TokenBase> tokenize = textTokenizer.tokenize(text);
+        return tokenize.stream().map(tokenBase -> {
+            final String[] features = tokenBase.getAllFeaturesArray();
+            final String katakana = toKatakanaPronunciation ? features[PRONUNCIATION_ID] : features[READING_ID];
+            return katakana.equals("*") ? tokenBase.getSurface() : katakana;
+        });
+    }
+
+}
