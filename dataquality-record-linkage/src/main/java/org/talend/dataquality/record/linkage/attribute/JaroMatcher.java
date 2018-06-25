dbe93d4e3ae6445d72022bf2680715684ce16727
qiongli <qiongli@talend.com>
2018-06-25 17:52:54 +0800

    feat(TDQ-15079) code format

--- dataquality-record-linkage/src/main/java/org/talend/dataquality/record/linkage/attribute/JaroMatcher.java
+++ dataquality-record-linkage/src/main/java/org/talend/dataquality/record/linkage/attribute/JaroMatcher.java
@@ -42,9 +42,8 @@ public class JaroMatcher extends AbstractAttributeMatcher {
     public double getWeight(String string1, String string2) {
 
         // get half the length of the string rounded up - (this is the distance used for acceptable transpositions)
-        final int halflen =
-                ((Math.min(string1.length(), string2.length())) / 2)
-                        + ((Math.min(string1.length(), string2.length())) % 2);
+        final int halflen = ((Math.min(string1.length(), string2.length())) / 2)
+                + ((Math.min(string1.length(), string2.length())) % 2);
 
         // get common characters
         final StringBuilder common1 = StringComparisonUtil.getCommonCharacters(string1, string2, halflen);
@@ -69,8 +68,8 @@ public class JaroMatcher extends AbstractAttributeMatcher {
         transpositions /= 2.0f;
 
         // calculate jaro metric
-        return (common1.length() / ((float) string1.length()) + common2.length() / ((float) string2.length()) + (common1
-                .length() - transpositions) / ((float) common1.length())) / 3.0f;
+        return (common1.length() / ((float) string1.length()) + common2.length() / ((float) string2.length())
+                + (common1.length() - transpositions) / ((float) common1.length())) / 3.0f;
     }
 
 }
