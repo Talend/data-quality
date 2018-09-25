4ec9370cfa485c25cb0ab4b19d5b927ff63ba5a4
talend-vlesquere <vlesquere@talend.com>
2018-09-25 17:21:54 +0200

    change map naming

--- dataquality-text-japanese/src/main/java/org/talend/dataquality/jp/transliteration/KatakanaToHiragana.java
+++ dataquality-text-japanese/src/main/java/org/talend/dataquality/jp/transliteration/KatakanaToHiragana.java
@@ -14,7 +14,6 @@ package org.talend.dataquality.jp.transliteration;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.Function;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -71,9 +70,9 @@ public class KatakanaToHiragana {
         char[] katakanaTokenchars = katakanaToken.toCharArray();
         for (int i = 0; i < katakanaTokenchars.length; i++) {
             char katakanaChar = katakanaTokenchars[i];
-            Character zenkakuChar = MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.get(katakanaChar);
-            if (zenkakuChar != null) {
-                katakanaTokenchars[i] = zenkakuChar;
+            Character fullWidthChar = MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.get(katakanaChar);
+            if (fullWidthChar != null) {
+                katakanaTokenchars[i] = fullWidthChar;
             }
         }
 
@@ -104,73 +103,74 @@ public class KatakanaToHiragana {
         return (('\u30a1' <= c) && (c <= '\u30fe'));
     }
 
-    protected static final Map<Character, Character> MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA;
+    //Mapping HANKAKU TO ZENKAKU
+    private static final Map<Character, Character> MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA;
     static {
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA = new HashMap();
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('｡', '。');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('｢', '「');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('｣', '」');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('､', '、');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('･', '・');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ｧ', 'ァ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ｨ', 'ィ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ｩ', 'ゥ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ｪ', 'ェ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ｫ', 'ォ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ｬ', 'ャ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ｭ', 'ュ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ｮ', 'ョ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ｯ', 'ッ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ｰ', 'ー');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ｱ', 'ア');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ｲ', 'イ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ｳ', 'ウ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ｴ', 'エ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ｵ', 'オ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ｶ', 'カ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ｷ', 'キ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ｸ', 'ク');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ｹ', 'ケ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ｺ', 'コ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ｻ', 'サ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ｼ', 'シ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ｽ', 'ス');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ｾ', 'セ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ｿ', 'ソ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ﾀ', 'タ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ﾁ', 'チ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ﾂ', 'ツ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ﾃ', 'テ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ﾄ', 'ト');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ﾅ', 'ナ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ﾆ', 'ニ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ﾇ', 'ヌ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ﾈ', 'ネ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ﾉ', 'ノ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ﾊ', 'ハ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ﾋ', 'ヒ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ﾌ', 'フ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ﾍ', 'ヘ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ﾎ', 'ホ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ﾏ', 'マ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ﾐ', 'ミ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ﾑ', 'ム');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ﾒ', 'メ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ﾓ', 'モ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ﾔ', 'ヤ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ﾕ', 'ユ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ﾖ', 'ヨ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ﾗ', 'ラ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ﾘ', 'リ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ﾙ', 'ル');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ﾚ', 'レ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ﾛ', 'ロ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ﾜ', 'ワ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ヮ', 'ヮ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ｴ', 'ヱ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ｦ', 'ヲ');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ﾝ', 'ン');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ﾞ', '゛');
-        MAPPING_HANKAKU_TO_ZENKAKU_KATAKANA.put('ﾟ', '゜');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA = new HashMap();
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('｡', '。');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('｢', '「');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('｣', '」');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('､', '、');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('･', '・');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ｧ', 'ァ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ｨ', 'ィ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ｩ', 'ゥ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ｪ', 'ェ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ｫ', 'ォ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ｬ', 'ャ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ｭ', 'ュ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ｮ', 'ョ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ｯ', 'ッ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ｰ', 'ー');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ｱ', 'ア');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ｲ', 'イ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ｳ', 'ウ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ｴ', 'エ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ｵ', 'オ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ｶ', 'カ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ｷ', 'キ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ｸ', 'ク');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ｹ', 'ケ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ｺ', 'コ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ｻ', 'サ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ｼ', 'シ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ｽ', 'ス');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ｾ', 'セ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ｿ', 'ソ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ﾀ', 'タ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ﾁ', 'チ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ﾂ', 'ツ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ﾃ', 'テ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ﾄ', 'ト');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ﾅ', 'ナ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ﾆ', 'ニ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ﾇ', 'ヌ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ﾈ', 'ネ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ﾉ', 'ノ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ﾊ', 'ハ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ﾋ', 'ヒ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ﾌ', 'フ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ﾍ', 'ヘ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ﾎ', 'ホ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ﾏ', 'マ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ﾐ', 'ミ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ﾑ', 'ム');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ﾒ', 'メ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ﾓ', 'モ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ﾔ', 'ヤ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ﾕ', 'ユ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ﾖ', 'ヨ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ﾗ', 'ラ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ﾘ', 'リ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ﾙ', 'ル');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ﾚ', 'レ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ﾛ', 'ロ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ﾜ', 'ワ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ヮ', 'ヮ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ｴ', 'ヱ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ｦ', 'ヲ');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ﾝ', 'ン');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ﾞ', '゛');
+        MAPPING_HALFWIDTH_TO_FULLWIDTH_KATAKANA.put('ﾟ', '゜');
     }
 }
