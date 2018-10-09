3e2f9a0a833d7d446816beb83610fd28b4c4fda6
Sizhao Liu <sizhaoliu@talend.com>
2018-10-10 01:22:32 +0300

    use commons-lang3 insteand of commons-lang

--- dataquality-semantic/src/main/java/org/talend/dataquality/semantic/datamasking/SemanticMaskerFunctionFactory.java
+++ dataquality-semantic/src/main/java/org/talend/dataquality/semantic/datamasking/SemanticMaskerFunctionFactory.java
@@ -16,7 +16,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Random;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.talend.dataquality.datamasking.FunctionFactory;
