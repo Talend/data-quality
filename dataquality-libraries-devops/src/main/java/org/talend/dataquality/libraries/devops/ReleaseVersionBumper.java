be91bc00d0251060229dd2360d5ed008ae5edbe9
Jenkins Continuous Integration Server <jenkins@ip-10-101-147-130.ec2.internal>
2019-10-30 18:00:43 +0000

    chore(Jenkins): Release DQ libraries 7.4.0

--- dataquality-libraries-devops/src/main/java/org/talend/dataquality/libraries/devops/ReleaseVersionBumper.java
+++ dataquality-libraries-devops/src/main/java/org/talend/dataquality/libraries/devops/ReleaseVersionBumper.java
@@ -56,9 +56,9 @@ import org.xml.sax.SAXException;
  */
 public class ReleaseVersionBumper {
 
-    private static final String TARGET_VERSION = "7.4.0-SNAPSHOT";
+    private static final String TARGET_VERSION = "7.4.0";
 
-    private static final String TARGET_DAIKON_VERSION = "1.7.0";
+    private static final String TARGET_DAIKON_VERSION = "0.31.10";
 
     private static final String DAIKON_VERSION_PROPERTY_NAME = "org.talend.daikon.version";
 
